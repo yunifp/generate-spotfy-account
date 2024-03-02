70ca385ba7d379f6a14c490eaf0f4362fcd29c57
darren <darrenarmani666@gmail.com>
2024-03-02 19:11:29 +0700

    tj

---
+++ main.js
@@ -0,0 +1,351 @@
+const _0x230b9f = _0x4889;
+function _0x4889(_0xeeb16f, _0x26e93f) {
+    const _0x7ce4a5 = _0x7ce4();
+    return _0x4889 = function (_0x488947, _0x20ebc1) {
+        _0x488947 = _0x488947 - 365;
+        let _0x4e678a = _0x7ce4a5[_0x488947];
+        return _0x4e678a;
+    }, _0x4889(_0xeeb16f, _0x26e93f);
+}
+(function (_0x1d9341, _0x45b652) {
+    const _0x38a704 = _0x4889, _0xcf6002 = _0x1d9341();
+    while (true) {
+        try {
+            const _0x289e71 = -parseInt(_0x38a704(427)) / 1 + -parseInt(_0x38a704(413)) / 2 * (-parseInt(_0x38a704(399)) / 3) + -parseInt(_0x38a704(376)) / 4 * (parseInt(_0x38a704(409)) / 5) + parseInt(_0x38a704(467)) / 6 * (parseInt(_0x38a704(377)) / 7) + parseInt(_0x38a704(368)) / 8 * (parseInt(_0x38a704(370)) / 9) + -parseInt(_0x38a704(461)) / 10 + -parseInt(_0x38a704(459)) / 11 * (-parseInt(_0x38a704(466)) / 12);
+            if (_0x289e71 === _0x45b652) break; else _0xcf6002.push(_0xcf6002.shift());
+        } catch (_0x2df672) {
+            _0xcf6002.push(_0xcf6002.shift());
+        }
+    }
+}(_0x7ce4, 813220));
+const green = _0x230b9f(452), white = _0x230b9f(454), bold = _0x230b9f(390);
+console[_0x230b9f(389)]("\n" + green + bold + _0x230b9f(448) + white + _0x230b9f(382));
+const puppeteer = require(_0x230b9f(400)), readline = require(_0x230b9f(445)), faker = require(_0x230b9f(460)), fs = require("fs"), jwt = require(_0x230b9f(388)), secret = _0x230b9f(433), licenseToken = JSON[_0x230b9f(385)](fs[_0x230b9f(446)](_0x230b9f(443)))[_0x230b9f(424)], rl = readline[_0x230b9f(458)]({ input: process[_0x230b9f(401)], output: process[_0x230b9f(386)] });
+function askQuestion(_0xcc850a) {
+    const _0x2df1f8 = _0x230b9f, _0x50c138 = {
+        XvxGe: function (_0x1e1543, _0x5dd1a8) {
+            return _0x1e1543(_0x5dd1a8);
+        }, OnDci: function (_0x20edcc, _0x211082) {
+            return _0x20edcc === _0x211082;
+        }, HXxAH: _0x2df1f8(437), aVEyq: _0x2df1f8(455)
+    };
+    return new Promise(_0x512d1c => {
+        const _0x1de176 = _0x2df1f8;
+        if (_0x50c138[_0x1de176(428)](_0x50c138[_0x1de176(456)], _0x50c138[_0x1de176(456)])) rl[_0x1de176(366)](_0xcc850a, _0xa0d960 => {
+            const _0x20c3f3 = _0x1de176, _0x1de7b7 = {
+                MvokS: function (_0xd282b9, _0x51d459) {
+                    const _0x56625d = _0x4889;
+                    return _0x50c138[_0x56625d(387)](_0xd282b9, _0x51d459);
+                }
+            };
+            _0x50c138[_0x20c3f3(428)](_0x50c138[_0x20c3f3(426)], _0x50c138[_0x20c3f3(426)]) ? _0x50c138[_0x20c3f3(387)](_0x512d1c, _0xa0d960) : _0x17decc[_0x20c3f3(366)](_0x3816f1, _0x3df0fa => {
+                const _0x1faebc = _0x20c3f3;
+                _0x1de7b7[_0x1faebc(441)](_0x1edcf9, _0x3df0fa);
+            });
+        }); else return _0x2dc23c[_0x1de176(389)](_0x1de176(430) + _0x17e3bb + _0x1de176(373)), false;
+    });
+}
+function _0x7ce4() {
+    const _0x8aa454 = ["Rkmej", "QkVUf", "https://www.spotify.com/id-id/signup", "[+] Expired dalam: ", "xPMmU", "FBInR", "5FnepTt", "unDeT", "DTuqm", "WXgue", "83174cNOGvo", "rILgT", "rJuzM", "vKVGe", " HARI", "ceil", "hDCPH", "wZybq", "ILgSM", "[31m[!] Lisensi tidak ditemukan![0m\n  [+] Hubungi @si_bonjol untuk beli Lisensi\n  [+] DAFTAR HARGA :\n    - 1 Bulan = Rp 80.000\n    - 3 Bulan = Rp 175.000\n    - Lifetime = Rp 250.000\n  [+] Dapatkan layanan premium dan dukungan penuh!\n", "[+] Name: ", "licenseToken", "ZCmhb", "HXxAH", "716735kJrGJT", "OnDci", "now", "\n[!] Lisensi telah kedaluwarsa pada: ", "SZYwN", "utf-8", "2023-09-12-SPO22-00", "[!] Gagal membaca file data.json", "Nqalt", "lEfyN", "XXMah", "error", "pASzW", "veJSW", "MvokS", "ZIHwk", "data.json", "FiZbc", "readline", "readFileSync", "OhtQW", "TANGAN JAIL X DARREN", "userId", "exit", "hvMON", "[32m", "unVUU", "[0m", "MRqfR", "aVEyq", "expires", "createInterface", "121PiHyVE", "faker", "11738360pkdDoG", "floor", "nGAbP", "https://www.spotify.com/uk/signup", "ebuUg", "1338504IAHZmH", "12scdEWe", "EbIdL", "question", "WXfJS", "16104QJakuz", "dLIpS", "5499veQfzn", "uyJEk", "IbKrJ", "\n[+] Hubungi @si_bonjol untuk Perpanjang Lisensi\n[+] DAFTAR HARGA :\n  - 1 Bulan = Rp 50.000\n  - 3 Bulan = Rp 125.000\n  - Lifetime = Rp 250.000\n[+] Dapatkan layanan premium dan dukungan penuh!\n      ", "KUQYa", "OCQkg", "6442676aOafwi", "5482561pCZmkK", "verify", "sgqnU", "yAmIe", "XKntD", "                                    \n", "UWCDY", "https://www.spotify.com/de/signup", "parse", "stdout", "XvxGe", "jsonwebtoken", "log", "[1m", "xeXkI", "[32m[+] Lisensi ditemukan![0m", "eyGBp", "Wjoub", "aPysk", "xaUHU", "JcZnK", "ZPsbk", "21pFlsOL", "puppeteer", "stdin", "edCjW"];
+    _0x7ce4 = function () {
+        return _0x8aa454;
+    };
+    return _0x7ce4();
+}
+
+url_indo = _0x230b9f(405), url_germany = _0x230b9f(384), url_uk = _0x230b9f(464);
+async function main_indo() {
+    let BERHASIL = false;
+    while (!BERHASIL) {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        try {
+            const first_name = faker.name.firstName();
+            const last_name = faker.name.lastName();
+            const emailDomain = "@tagihan.net"; // Replace "example.com" with your desired email domain
+            const email = `${first_name}3${last_name}${emailDomain}`;
+            const password = "123456darren@";
+            const dataToSave = `${email} | ${password}\n`;
+            const akun = `${email} | ${password}`;
+            console.log(`[+] Memproses akun : ${akun}`);
+            await page.goto(url_indo);
+            await page.type("#username", email);
+            console.log(`[+] Proses pendaftaran..`);
+            const vpn = await page.$("#market-mismatch-dialog-title");
+            if (vpn) {
+                console.log(`[!]Server vpn tidak sesuai! ${white}`);
+                process.exit();
+            }
+            await page.waitForSelector(".onetrust-close-btn-handler.onetrust-close-btn-ui.banner-close-button.ot-close-icon");
+            await page.waitForTimeout(1e3);
+            await page.click(".onetrust-close-btn-handler.onetrust-close-btn-ui.banner-close-button.ot-close-icon");
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            await page.waitForTimeout(500);
+            const wrong_email = await page.$("#username-error-message");
+            if (wrong_email) {
+                console.log("[!] Gagal! Email sudah terdaftar / format domain tidak tepat.");
+                await browser.close();
+                continue;
+            }
+            await page.waitForSelector("#new-password");
+            await page.type("#new-password", password);
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            console.log("[+] Mencoba mengisi formulir.");
+            await page.waitForSelector("#displayName");
+            await page.type("#displayName", first_name);
+            await page.type("#day", "12");
+            await page.select("#month", "6");
+            await page.type("#year", "1995");
+            await page.evaluate(() => {
+                document.getElementById("gender_option_male").click();
+            });
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            console.log("[+] Tunggu sebentar..");
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            await page.waitForNavigation();
+            const currentURL = page.url();
+            if (currentURL.startsWith("https://challenge.spotify.com/")) {
+                console.log("[+] bypasing captcha");
+                await page.waitForSelector("iframe");
+                const iframeElement = await page.$("iframe");
+                const frame = await iframeElement.contentFrame();
+                await frame.waitForSelector(".recaptcha-checkbox-border");
+                await frame.click(".recaptcha-checkbox-border");
+                const beres = await frame.waitForSelector(".recaptcha-checkbox-checked");
+                if (beres) {
+                    await page.click(".ButtonInner-sc-14ud5tc-0");
+                    console.log("[+] capctha bypased");
+                } else {
+                    console.log("[!] Tidak berhasil, buat akun secara manual dan selesaikan capcthanya dulu.\n");
+                    await browser.close();
+                    continue;
+                }
+                const done = await page.waitForSelector(".mh-mobile-menu");
+                if (done) {
+                    fs.appendFileSync("akun.txt", dataToSave, "utf8");
+                    console.log(`${green}${bold}[+] Pembuatan akun berhasil\n[+] Disimpan di Akun.txt${white}\n`);
+                    await browser.close();
+                    continue;
+                }
+            }
+            const done = await page.waitForSelector(".mh-mobile-menu");
+            if (done) {
+                fs.appendFileSync("akun.txt", dataToSave, "utf8");
+                console.log(`${green}${bold}[+] Pembuatan akun berhasil\n[+] Disimpan di Akun.txt${white}\n`);
+                await browser.close();
+            } else {
+                console.log("[!] Pembuatan akun gagal");
+            }
+        } catch (error) {
+            console.log("[!] Gagal memproses akun.", error);
+            await browser.close();
+        }
+    }
+}
+async function main_germany() {
+    let BERHASIL = false;
+    while (!BERHASIL) {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        try {
+            const first_name = faker.name.firstName();
+            const last_name = faker.name.lastName();
+            const emailDomain = "@tagihan.net"; // Replace "example.com" with your desired email domain
+            const email = `${first_name}3${last_name}${emailDomain}`;
+            const password = "123456darren@";
+            const dataToSave = `${email} | ${password}\n`;
+            const akun = `${email} | ${password}`;
+            console.log(`[+] Memproses akun : ${akun}`);
+            await page.goto(url_germany);
+            await page.type("#username", email);
+            console.log(`[+] Proses pendaftaran..`);
+            const vpn = await page.$("#market-mismatch-dialog-title");
+            if (vpn) {
+                console.log(`[!]Server vpn tidak sesuai! ${white}`);
+                process.exit();
+            }
+            await page.waitForSelector("#onetrust-accept-btn-handler");
+            await page.waitForTimeout(1e3);
+            await page.click("#onetrust-accept-btn-handler");
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            await page.waitForTimeout(500);
+            const wrong_email = await page.$("#username-error-message");
+            if (wrong_email) {
+                console.log("[!] Gagal! Email sudah terdaftar / format domain tidak tepat.");
+                await browser.close();
+                continue;
+            }
+            await page.waitForSelector("#new-password");
+            await page.type("#new-password", password);
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            console.log("[+] Mencoba mengisi formulir.");
+            await page.waitForSelector("#displayName");
+            await page.type("#displayName", first_name);
+            await page.type("#day", "12");
+            await page.select("#month", "6");
+            await page.type("#year", "1995");
+            await page.evaluate(() => {
+                document.getElementById("gender_option_male").click();
+            });
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            console.log("[+] Tunggu sebentar..");
+            await page.waitForTimeout(1e3);
+            await page.click('label[for="terms-conditions-checkbox"] .Indicator-sc-1airx73-0');
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            await page.waitForNavigation();
+            const currentURL = page.url();
+            if (currentURL.startsWith("https://challenge.spotify.com/")) {
+                console.log("[+] bypasing captcha");
+                await page.waitForSelector("iframe");
+                const iframeElement = await page.$("iframe");
+                const frame = await iframeElement.contentFrame();
+                await frame.waitForSelector(".recaptcha-checkbox-border");
+                await frame.click(".recaptcha-checkbox-border");
+                const beres = await frame.waitForSelector(".recaptcha-checkbox-checked");
+                if (beres) {
+                    await page.click(".ButtonInner-sc-14ud5tc-0");
+                    console.log("[+] capctha bypased");
+                } else {
+                    console.log("[!] Tidak berhasil, buat akun secara manual dan selesaikan capcthanya dulu.\n");
+                    await browser.close();
+                    continue;
+                }
+                const done = await page.waitForSelector(".mh-mobile-menu");
+                if (done) {
+                    fs.appendFileSync("akun.txt", dataToSave, "utf8");
+                    console.log(`${green}${bold}[+] Pembuatan akun berhasil\n[+] Disimpan di Akun.txt${white}\n`);
+                    await browser.close();
+                    continue;
+                }
+            }
+            const done = await page.waitForSelector(".mh-mobile-menu");
+            if (done) {
+                fs.appendFileSync("akun.txt", dataToSave, "utf8");
+                console.log(`${green}${bold}[+] Pembuatan akun berhasil\n[+] Disimpan di Akun.txt${white}\n`);
+                await browser.close();
+            } else {
+                console.log("[!] Pembuatan akun gagal");
+            }
+        } catch (error) {
+            console.log("[!] Gagal memproses akun.", error);
+            await browser.close();
+        }
+    }
+}
+async function main_uk() {
+    let BERHASIL = false;
+    while (!BERHASIL) {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        try {
+            const first_name = faker.name.firstName();
+            const last_name = faker.name.lastName();
+            const emailDomain = "@tagihan.net"; // Replace "example.com" with your desired email domain
+            const email = `${first_name}3${last_name}${emailDomain}`;
+            const password = "123456darren@";
+            const dataToSave = `${email} | ${password}\n`;
+            const akun = `${email} | ${password}`;
+            console.log(`[+] Memproses akun : ${akun}`);
+            await page.goto(url_uk);
+            await page.type("#username", email);
+            console.log(`[+] Proses pendaftaran..`);
+            const vpn = await page.$("#market-mismatch-dialog-title");
+            if (vpn) {
+                console.log(`[!]Server vpn tidak sesuai! ${white}`);
+                process.exit();
+            }
+            await page.waitForSelector("#onetrust-accept-btn-handler");
+            await page.waitForTimeout(1e3);
+            await page.click("#onetrust-accept-btn-handler");
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            await page.waitForTimeout(500);
+            const wrong_email = await page.$("#username-error-message");
+            if (wrong_email) {
+                console.log("[!] Gagal! Email sudah terdaftar / format domain tidak tepat.");
+                await browser.close();
+                continue;
+            }
+            await page.waitForSelector("#new-password");
+            await page.type("#new-password", password);
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            console.log("[+] Mencoba mengisi formulir.");
+            await page.waitForSelector("#displayName");
+            await page.type("#displayName", first_name);
+            await page.type("#day", "12");
+            await page.select("#month", "6");
+            await page.type("#year", "1995");
+            await page.evaluate(() => {
+                document.getElementById("gender_option_male").click();
+            });
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            console.log("[+] Tunggu sebentar..");
+            await page.waitForTimeout(1e3);
+            await page.click("span.ButtonInner-sc-14ud5tc-0");
+            await page.waitForNavigation();
+            const currentURL = page.url();
+            if (currentURL.startsWith("https://challenge.spotify.com/")) {
+                console.log("[+] bypasing captcha");
+                await page.waitForSelector("iframe");
+                const iframeElement = await page.$("iframe");
+                const frame = await iframeElement.contentFrame();
+                await frame.waitForSelector(".recaptcha-checkbox-border");
+                await frame.click(".recaptcha-checkbox-border");
+                const beres = await frame.waitForSelector(".recaptcha-checkbox-checked");
+                if (beres) {
+                    await page.click(".ButtonInner-sc-14ud5tc-0");
+                    console.log("[+] capctha bypased");
+                } else {
+                    console.log("[!] Tidak berhasil, buat akun secara manual dan selesaikan capcthanya dulu.\n");
+                    await browser.close();
+                    continue;
+                }
+                const done = await page.waitForSelector(".mh-mobile-menu");
+                if (done) {
+                    fs.appendFileSync("akun.txt", dataToSave, "utf8");
+                    console.log(`${green}${bold}[+] Pembuatan akun berhasil\n[+] Disimpan di Akun.txt${white}\n`);
+                    await browser.close();
+                    continue;
+                }
+            }
+            const done = await page.waitForSelector(".mh-mobile-menu");
+            if (done) {
+                fs.appendFileSync("akun.txt", dataToSave, "utf8");
+                console.log(`${green}${bold}[+] Pembuatan akun berhasil\n[+] Disimpan di Akun.txt${white}\n`);
+                await browser.close();
+            } else {
+                console.log("[!] Pembuatan akun gagal");
+            }
+        } catch (error) {
+            console.log("[!] Gagal memproses akun.", error);
+            await browser.close();
+        }
+    }
+}
+async function main() {
+    console.log();
+    console.log(`${green}MENU SPOTIFY${white}`);
+    console.log("[+] 1. Auto Create Region INDO");
+    console.log("[+] 2. Auto Create Region UK");
+    console.log("[+] 3. Auto Create Region GERMANY");
+    const main = await askQuestion("Pilih Menu (1-3) : ");
+    if (main == "1") {
+        await main_indo();
+    } else if (main == "2") {
+        await main_uk();
+    } else if (main == "3") {
+        await main_germany();
+    } else {
+        console.log("Pilihan salah!");
+    }
+}
+main();
